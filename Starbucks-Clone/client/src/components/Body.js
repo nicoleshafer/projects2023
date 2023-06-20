5b4e7a49442fdb91deab76b31633f94a5e83e102
Nicole Shafer <nicolershafer@gmail.com>
2023-06-20 11:10:15 -0400

    added body divs

--- Starbucks-Clone/client/src/components/Body.js
+++ Starbucks-Clone/client/src/components/Body.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import summer from "../body-images/summer-img.jpg"
+import sweets from "../body-images/sweets.jpg"
 
 const Body = () => {
     return (
         <div>
-            <div className='container'></div>
+            <div className='container'>
+                <div className='bodyOuterBox boxOne'>
+                    <div>
+                        <img src={summer} alt='fraps' className='bodyImage'></img>
+                    </div>
+                    <div className='boxText'>
+                        <h2>Turn this summer<br/>up</h2>
+                        <p>
+                        Here’s to the new Chocolate Java Mint 
+                        <br />Frappuccino® blended beverage and classic 
+                        <br/>Caramel Ribbon Crunch Frappuccino®
+                        <br/> blended beverage.
+                        </p>
+                    </div>
+                </div>
+
+                <div className='bodyOuterBox boxTwo'>
+                <div className='boxText'>
+                    <h2>Sweetest of the hive</h2>
+                </div>
+                    <div>
+                        <img src={sweets} alt="pops" className='bodyImage'></img>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 }
