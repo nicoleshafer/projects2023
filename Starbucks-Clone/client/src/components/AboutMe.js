e7c0a7a29ebfc6dc08279f8fdeabf3c27afe524b
Nicole Shafer <nicolershafer@gmail.com>
2023-06-20 11:56:14 -0400

    updates 6/20

---
+++ Starbucks-Clone/client/src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div>
+            Hi, I'm Nicole. Welcome to my Starbucks Clone!
+        </div>
+    );
+}
+
+export default AboutMe;
--- Starbucks-Clone/client/src/components/Homepage.js
+++ Starbucks-Clone/client/src/components/Homepage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../components/Nav';
 import Body from "../components/Body"
+import Footer from './Footer';
 
 const Homepage = () => {
     return (
         <div>
             <Nav />
             <Body/>
+            <Footer />
         </div>
     );
 }
